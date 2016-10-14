5bc62bb5b7224502e710b6afd4fb7a32fe12dadb
Michael Freese <mrfreese1@gmail.com>
2016-10-14 10:41:57 -0400

    second

--- blackjack.rb
+++ blackjack.rb
@@ -1,7 +1,7 @@
 require_relative 'deck'
 class BlackJack
 
-attr_accessor :game, :player_hand, :dealer_hand, :full_deck :player_score, :dealer_score
+attr_accessor :game, :player_hand, :dealer_hand, :full_deck, :player_score, :dealer_score
 
 def initialize (full_deck, player_hand, dealer_hand)
 self.full_deck = Deck.new
@@ -11,8 +11,7 @@ end
 
   def play
     puts "To play BlackJack press enter"
-    make deck
-    deal
+    deal_hand
     round
     winner
     rematch
@@ -25,8 +24,16 @@ end
     end
   end
 def round
-  
-
+  player_hand
+  deal_hand
+  show_hand
+  puts "You have #{player_score} with #{player_hand}"
+  puts "Dealer has a #{dealer_score} with #{dealer_hand}"
+  unless
+    blackjack || bust
+    puts "Do You Want To Hit or Stay?"
+  end
+end
 end
 def player_total
   self.player_total = player_hand.reduce (0){ |sum, card| sum + card.value }
@@ -34,9 +41,20 @@ end
 def dealer_total
   self.dealer_total = dealer_hand.reduce (0){ |sum, card| sum + card.value}
 end
+def blackjack
+  player_value blackjack == 21
+end
+def bust
+  player_score > 21
+end
+def dealer_bust
+  dealer_bust > 21
+end
+def player_hit
+  player_hit = 
 
 end
-
+end
 
 
 end
